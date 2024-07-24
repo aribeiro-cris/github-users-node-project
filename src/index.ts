93f5473d7ac131ee76399f16334cee58c7bcff0e
Andreia <andreiacribeiro.5@gmail.com>
2024-07-24 14:09:25 +0100

    query per language and per location done

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import fetchUserFromGithub from './fetchService.js'; 
-import {saveUser, showAllUsers} from './userService.js';
+import {saveUser, showAllUsers, showUsersPerLocation, showUsersPerLanguage} from './userService.js';
 
 //command line args
 const args = process.argv.slice(2); //creates an array with the args passing starting on index 2 (third element)
 
 if (args.length === 0) {
   console.log('Please provide some input on the command line.');
-  showAllUsers();
+  //showAllUsers();
+  //showUsersPerLocation('Lisbon');
+  showUsersPerLanguage('TypeScript');
 } else {
   const username = args[0];
 
@@ -22,6 +24,5 @@ if (args.length === 0) {
       }
       
     }
-  }
-  )
+  });
 }
